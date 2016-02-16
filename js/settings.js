35be7d065b8ccd273a2c87d7fd50b1d9f2f82bfd
scooterpsu <scooterpsu@gmail.com>
2016-02-16 08:08:48 -0500

    JS cleanup, changed rcon call to verify setting before writing config needlessly

--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,8 @@
-var settings = [];
-
 var settingsToLoad = [['pName', 'Player.Name'], ['renderWeapon', 'Player.RenderWeapon'], ['armorHelmet', 'Player.Armor.Helmet'], ['armorChest', 'Player.Armor.Chest'], ['armorShoulders', 'Player.Armor.Shoulders'], ['armorArms', 'Player.Armor.Arms'], ['armorLegs', 'Player.Armor.Legs '], ['colorsPrimary', 'Player.Colors.Primary'], ['colorsSecondary', 'Player.Colors.Secondary'], ['colorsVisor', 'Player.Colors.Visor'], ['colorsLights', 'Player.Colors.Lights'], ['colorsHolo', 'Player.Colors.Holo'],['sName', 'Server.Name'],['sCountdown', 'Server.Countdown'], ['sMaxPlayers', 'Server.MaxPlayers'],['sShouldAnnounce', 'Server.ShouldAnnounce'],['sSprintEnabled', 'Server.SprintEnabled'], ['sUnlimitedSprint', 'Server.UnlimitedSprint'], ['sDualWieldEnabled', 'Server.DualWieldEnabled'], ['sAssassinationEnabled', 'Server.AssassinationEnabled'], ['cCenteredCrosshair' , 'Camera.Crosshair'], ['cFOV', 'Camera.FOV'], ['cHideHUD', 'Camera.HideHUD'], ['cSpeed', 'Camera.Speed'],['inputRaw','Input.RawInput'], ['voipPTT', 'VoIP.PushToTalk'], ['voipVolMod', 'VoIP.VolumeModifier'], ['voipAGC', 'VoIP.AGC'], ['voipEchoCancel', 'VoIP.EchoCancellation'], ['voipVAL', 'VoIP.VoiceActivationLevel'], ['voipServerEnable', 'VoIP.ServerEnabled'], ['voipEnabled', 'VoIP.Enabled'], ['gfxSaturation', 'Graphics.Saturation'], ['gfxBloom', 'Graphics.Bloom'], ['sPass', 'Server.Password']];
 var loadedSettings = false;
 
 $(document).ready(function() {
     fixResolution();
-    setInterval( CheckPageFocus, 200 );
     $('#chest').append($("#helmet > option").clone());
     $('#shoulders').append($("#helmet > option").clone());
     $('#arms').append($("#helmet > option").clone());
@@ -16,21 +13,9 @@ $(document).ready(function() {
 function fixResolution() {
     zoomRatio = screen.width/1920;
     $('#settingsWindow').css("zoom", zoomRatio);  
-  
-}
-
-function CheckPageFocus() {
-  //var info = document.getElementById("message");
-
-  if ( document.hasFocus() ) {
-    pageFocus = true;
-  } else {
-    pageFocus = false;
-  }
 }
 
 function connectionTrigger() {
-    console.log("connected");
     loadSettings(0);
 }
 
@@ -39,16 +24,15 @@ function loadSettings(i) {
 		dewRcon.send(settingsToLoad[i][1], function(ret) {
             if(settingsToLoad[i][1].startsWith("Player.Colors")){
                 $("input[name='"+settingsToLoad[i][0]+"']").css("background-color",ret);                   
-            } else {
-                if ($("input[name='"+settingsToLoad[i][0]+"']").is(':checkbox')){
-                    if (ret == "1"){
-                        $("input[name='"+settingsToLoad[i][0]+"']").prop('checked', true);
-                    }                
-                }else{
-                    $("input[name='"+settingsToLoad[i][0]+"']").val(ret);
-                }
-                $("select[name='"+settingsToLoad[i][0]+"']").val(ret);
+            }             
+            if ($("input[name='"+settingsToLoad[i][0]+"']").is(':checkbox')){
+                if (ret == "1"){
+                    $("input[name='"+settingsToLoad[i][0]+"']").prop('checked', true);
+                }                
+            }else{
+                $("input[name='"+settingsToLoad[i][0]+"']").val(ret);
             }
+            $("select[name='"+settingsToLoad[i][0]+"']").val(ret);
 			i++;
 			loadSettings(i);
 		});
@@ -65,12 +49,25 @@ function updateSetting(thing, value){
             value = "0";
         }
     }
-    console.log(thing + ": " + value);
     if (value.length < 1){
         value = "\"\"";
     }
-    dewRcon.send(settingsToLoad[arrayInArray(thing, settingsToLoad)][1] + " " + value);
-    dewRcon.send("writeconfig");
+    dewRcon.send(settingsToLoad[arrayInArray(thing, settingsToLoad)][1] + " " + value, function(res){
+        if (res != "Command/Variable not found") {
+            dewRcon.send("writeconfig");
+        }
+    });
+}
+
+function closeBrowser() {
+	if(dewRconConnected) {
+		setTimeout(function() {
+			dewRcon.send('menu.show');
+			dewRcon.send('Game.SetMenuEnabled 0');
+		}, "500");
+	} else{
+		window.close();
+	}
 }
 
 function arrayInArray(needle, haystack) {
@@ -81,19 +78,6 @@ function arrayInArray(needle, haystack) {
     }
 }
 
-function closeBrowser() {
-	if(dewRconConnected) {
-		setTimeout(function() {
-			dewRcon.send('menu.show');
-			dewRcon.send('Game.SetMenuEnabled 0');
-		}, "1000");
-	} else{
-		window.close();
-	}
-}
-
-
-String.prototype.startsWith = function(needle)
-{
+String.prototype.startsWith = function(needle){
     return(this.indexOf(needle) == 0);
 };
